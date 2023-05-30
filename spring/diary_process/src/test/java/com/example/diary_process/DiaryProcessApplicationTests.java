022ded62565081b5040e30868e75baa4318ca091
ramram222 <kim.ram454@gmail.com>
2023-05-30 21:44:45 +0900

    init: spring 프로젝트 생성

---
+++ spring/diary_process/src/test/java/com/example/diary_process/DiaryProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diary_process;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
